4bdbb9de63658581d28058f30400e30e28124891
Brad Akemi <mirado.akemi@gmail.com>
2023-07-29 15:03:44 +0300

    DRF for test
    mise en place d'une model pour test

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# pour test uniquement
+
+
+class Product(models.Model):
+
+    product_name = models.CharField(max_length=45)
+    slug = models.SlugField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Produit"
