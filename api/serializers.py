c1728baa1ac27d782edf22172b38cab0482c40a2
Brad Akemi <mirado.akemi@gmail.com>
2023-08-07 19:08:10 +0300

    api for category

--- api/serializers.py
+++ api/serializers.py
@@ -12,3 +12,9 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
